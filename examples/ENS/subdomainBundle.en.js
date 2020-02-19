713cc41fb6fcdda79483150be2b953d5080e1696
Weiwu Zhang <a@colourful.land>
2020-02-19 15:21:24 +1100

    seems Sangalli put the CDATA closing tag at wrong places

--- examples/ENS/subdomainBundle.en.js
+++ examples/ENS/subdomainBundle.en.js
@@ -1,5 +1,5 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 //<![CDATA[
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 class Token {
 
     constructor(tokenInstance) {
@@ -39,7 +39,6 @@ web3.tokens.dataChanged = (oldTokens, updatedTokens) => {
     //need to render elements before calling setup
     token.setup();
 };
-//]]>
 
 },{"eth-ens-namehash":2}],2:[function(require,module,exports){
 (function (Buffer){
@@ -4214,3 +4213,4 @@ process.umask = function() { return 0; };
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[1]);
+//]]>
